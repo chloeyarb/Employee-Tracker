c5a2b8c62dbd478710d71f17f51671280c153492
Chloe Yarborough <chloeyarborough@Chloes-MacBook-Air.local>
2022-03-25 11:43:39 -0400

    added tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,23 @@
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS employee;
+
 CREATE TABLE department (
     id INTEGER PRIMARY KEY, 
     name VARCHAR(30)
+);
+
+CREATE TABLE role (
+    id INTEGER PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INTEGER,
+);
+
+CREATE TABLE employee (
+    id INTEGER PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER
 );
